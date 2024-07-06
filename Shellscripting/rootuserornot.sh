e24740feae794923de3803c6f562554060918931
jhvreddy <100144454+jhvreddy@users.noreply.github.com>
2024-07-06 19:19:00 +0530

    Update rootuserornot.sh

--- Shellscripting/rootuserornot.sh
+++ Shellscripting/rootuserornot.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+echo "Please enter the username"
+read userName
+#reading the user name and assigning it to variable userName
+if [ `id -u $userName` -eq 0 ]
+#the uid value of the root user is zero which can be found from cat /etc/passwd
+then
+echo "the user is root user"
+else
+echo "the user is not root user"
+fi
 
